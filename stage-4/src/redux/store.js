fcbe0fae111ca778b9e341c2dc56a7dcb2449b7b
Matthew Bodily <matthewbodily@Matthews-MacBook-Air.local>
2019-02-05 14:54:53 -0700

    Stage 3 Finished and Stage 4 started

--- stage-4/src/redux/store.js
+++ stage-4/src/redux/store.js
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
 import reduxPromiseMiddleware from 'redux-promise-middleware';
+import applyMiddleware from 'redux-promise-middleware';
 
 export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
